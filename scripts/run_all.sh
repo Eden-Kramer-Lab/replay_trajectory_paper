374b7bae14bfe6d0794c958797f2fabc7f3812b4
Eric Denovellis <edeno@bu.edu>
2019-08-08 07:49:54 -0400

    Fix epoch

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -6,4 +6,4 @@ python queue_cluster_jobs.py --Animal 'remy' --Day 35 --Epoch 4 --data_type 'clu
 python queue_cluster_jobs.py --Animal 'remy' --Day 36 --Epoch 2 --data_type 'clusterless' --n_cores 1 --n_workers 1 --wall_time '00:30:00'
 python queue_cluster_jobs.py --Animal 'remy' --Day 36 --Epoch 4 --data_type 'clusterless' --n_cores 1 --n_workers 1 --wall_time '00:30:00'
 python queue_cluster_jobs.py --Animal 'remy' --Day 37 --Epoch 2 --data_type 'clusterless' --n_cores 1 --n_workers 1 --wall_time '00:30:00'
-python queue_cluster_jobs.py --Animal 'remy' --Day 37 --Epoch 5 --data_type 'clusterless' --n_cores 1 --n_workers 1 --wall_time '00:30:00'
+python queue_cluster_jobs.py --Animal 'remy' --Day 37 --Epoch 4 --data_type 'clusterless' --n_cores 1 --n_workers 1 --wall_time '00:30:00'
