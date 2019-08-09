715cec6da7537cd732f5a9a10d0850bca3f23f50
Eric Denovellis <edeno@bu.edu>
2019-08-09 16:42:49 -0400

    Comment out epochs that take longer

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -7,3 +7,7 @@ python queue_cluster_jobs.py --Animal 'remy' --Day 36 --Epoch 2 --data_type 'clu
 python queue_cluster_jobs.py --Animal 'remy' --Day 36 --Epoch 4 --data_type 'clusterless' --n_cores 1 --n_workers 1 --wall_time '00:30:00'
 python queue_cluster_jobs.py --Animal 'remy' --Day 37 --Epoch 2 --data_type 'clusterless' --n_cores 1 --n_workers 1 --wall_time '00:30:00'
 python queue_cluster_jobs.py --Animal 'remy' --Day 37 --Epoch 4 --data_type 'clusterless' --n_cores 1 --n_workers 1 --wall_time '00:30:00'
+
+
+# python queue_cluster_jobs.py --Animal 'fra' --Day 5 --Epoch 6 --data_type 'sorted_spikes' --n_cores 1 --n_workers 1 --wall_time '01:00:00'
+# python queue_cluster_jobs.py --Animal 'Cor' --Day 4 --Epoch 2 --data_type 'sorted_spikes' --n_cores 1 --n_workers 1 --wall_time '01:00:00'
