f6f0e2dca4a158a9f68b387bd3cdf21a7effc5c0
Eric Denovellis <edeno@bu.edu>
2019-08-09 12:45:21 -0400

    Ensure excluded state in mixtures is not larger than 1 - probability threshold

--- src/analysis.py
+++ src/analysis.py
@@ -133,12 +133,14 @@ def get_is_classified(probability, probablity_threshold):
     is_classified.loc[dict(state='hover-continuous-mix')] = (
         is_classified.sel(state='hover-continuous-mix') &
         ~is_classified.sel(state='hover') &
-        ~is_classified.sel(state='continuous'))
+        ~is_classified.sel(state='continuous') &
+        (probability.sel(state='fragmented') < 1 - probablity_threshold))
 
     is_classified.loc[dict(state='fragmented-continuous-mix')] = (
         is_classified.sel(state='fragmented-continuous-mix') &
         ~is_classified.sel(state='fragmented') &
-        ~is_classified.sel(state='continuous'))
+        ~is_classified.sel(state='continuous') &
+        (probability.sel(state='hover') < 1 - probablity_threshold))
     is_classified = is_classified.rename('is_classified')
     is_classified = is_classified.where(~np.isnan(probability))
 
