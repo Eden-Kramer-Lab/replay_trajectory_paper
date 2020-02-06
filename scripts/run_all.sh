2f00bcffe7107c1b36959bae3d46b46b42d8d859
Eric Denovellis <edeno@bu.edu>
2020-02-06 14:28:43 -0800

    Change order of run

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -5,7 +5,7 @@ SORTED_WALLTIME='5:00:00'
 CLUSTERLESS_WALLTIME='10:00:00'
 REMY_WALLTIME='14:00:00'
 
-python queue_cluster_jobs.py --data_type 'sorted_spikes' --n_cores $NCORES --n_workers $NWORKERS --wall_time $SORTED_WALLTIME --plot_ripple_figures
+# Clusterless
 python queue_cluster_jobs.py --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $CLUSTERLESS_WALLTIME --plot_ripple_figures
 python queue_cluster_jobs.py --Animal 'remy' --Day 35 --Epoch 2 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME --plot_ripple_figures
 python queue_cluster_jobs.py --Animal 'remy' --Day 35 --Epoch 4 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME --plot_ripple_figures
@@ -13,3 +13,6 @@ python queue_cluster_jobs.py --Animal 'remy' --Day 36 --Epoch 2 --data_type 'clu
 python queue_cluster_jobs.py --Animal 'remy' --Day 36 --Epoch 4 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME --plot_ripple_figures
 python queue_cluster_jobs.py --Animal 'remy' --Day 37 --Epoch 2 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME --plot_ripple_figures
 python queue_cluster_jobs.py --Animal 'remy' --Day 37 --Epoch 4 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME --plot_ripple_figures
+
+# Sorted Spikes
+python queue_cluster_jobs.py --data_type 'sorted_spikes' --n_cores $NCORES --n_workers $NWORKERS --wall_time $SORTED_WALLTIME --plot_ripple_figures
