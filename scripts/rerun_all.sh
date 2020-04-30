01ab356c5545c0469da4f9b0bb84a739e0cf93d7
Eric Denovellis <edeno@bu.edu>
2020-04-30 12:20:19 -0700

    Don't plot ripple figures on re-run

--- scripts/rerun_all.sh
+++ scripts/rerun_all.sh
@@ -6,13 +6,13 @@ CLUSTERLESS_WALLTIME='1:00:00'
 REMY_WALLTIME='1:00:00'
 
 # Clusterless
-python queue_cluster_jobs.py --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $CLUSTERLESS_WALLTIME --plot_ripple_figures
-python queue_cluster_jobs.py --Animal 'remy' --Day 35 --Epoch 2 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME --plot_ripple_figures
-python queue_cluster_jobs.py --Animal 'remy' --Day 35 --Epoch 4 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME --plot_ripple_figures
-python queue_cluster_jobs.py --Animal 'remy' --Day 36 --Epoch 2 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME --plot_ripple_figures
-python queue_cluster_jobs.py --Animal 'remy' --Day 36 --Epoch 4 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME --plot_ripple_figures
-python queue_cluster_jobs.py --Animal 'remy' --Day 37 --Epoch 2 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME --plot_ripple_figures
-python queue_cluster_jobs.py --Animal 'remy' --Day 37 --Epoch 4 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME --plot_ripple_figures
+python queue_cluster_jobs.py --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $CLUSTERLESS_WALLTIME
+python queue_cluster_jobs.py --Animal 'remy' --Day 35 --Epoch 2 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME
+python queue_cluster_jobs.py --Animal 'remy' --Day 35 --Epoch 4 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME
+python queue_cluster_jobs.py --Animal 'remy' --Day 36 --Epoch 2 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME
+python queue_cluster_jobs.py --Animal 'remy' --Day 36 --Epoch 4 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME
+python queue_cluster_jobs.py --Animal 'remy' --Day 37 --Epoch 2 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME
+python queue_cluster_jobs.py --Animal 'remy' --Day 37 --Epoch 4 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME
 
 # Sorted Spikes
-python queue_cluster_jobs.py --data_type 'sorted_spikes' --n_cores $NCORES --n_workers $NWORKERS --wall_time $SORTED_WALLTIME --plot_ripple_figures
+python queue_cluster_jobs.py --data_type 'sorted_spikes' --n_cores $NCORES --n_workers $NWORKERS --wall_time $SORTED_WALLTIME
