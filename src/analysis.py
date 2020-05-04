3b04bdcae0a9d7fa153e72aa2ff66b742dcf88cb
Eric Denovellis <edeno@bu.edu>
2020-05-04 14:45:35 -0700

    Add highest posterior density metrics

--- src/analysis.py
+++ src/analysis.py
@@ -283,6 +283,16 @@ def get_replay_distance_metrics(results, ripple_position_info, ripple_spikes,
     replay_velocity_center_well = np.gradient(
         replay_distance_from_center_well, time)
 
+    hpd_threshold = highest_posterior_density(
+        posterior.sum("state"), coverage=0.95)
+    isin_hpd = posterior.sum("state") >= hpd_threshold[:, np.newaxis]
+    spatial_coverage = (
+        isin_hpd * np.diff(posterior.position)[0]).sum("position").values
+    n_position_bins = (posterior.sum("state", skipna=True)
+                       > 0).sum("position").values[0]
+    spatial_coverage_percentage = (isin_hpd.sum("position") /
+                                   n_position_bins).values
+
     metrics = {
         'replay_distance_from_actual_position': np.mean(
             replay_distance_from_actual_position),
@@ -297,6 +307,8 @@ def get_replay_distance_metrics(results, ripple_position_info, ripple_spikes,
             np.abs(np.diff(replay_distance_from_actual_position))),
         'replay_total_displacement': replay_total_displacement,
         'state_order': get_state_order(is_classified),
+        'spatial_coverage': np.mean(spatial_coverage),
+        'spatial_coverage_percentage': np.mean(spatial_coverage_percentage),
     }
 
     for state, above_threshold in is_classified.groupby('state'):
@@ -332,6 +344,10 @@ def get_replay_distance_metrics(results, ripple_position_info, ripple_spikes,
             metrics[f'{state}_population_rate'] = (
                 sampling_frequency * metrics[f'{state}_n_total_spikes'] /
                 above_threshold.sum())
+            metrics[f'{state}_spatial_coverage'] = np.mean(
+                spatial_coverage[above_threshold])
+            metrics[f'{state}_spatial_coverage_percentage'] = np.mean(
+                spatial_coverage_percentage[above_threshold])
 
     return metrics
 
@@ -544,3 +560,41 @@ def calculate_replay_distance(track_graph, map_estimate, actual_positions,
 
     return (replay_distance_from_actual_position,
             replay_distance_from_center_well)
+
+
+def highest_posterior_density(posterior_density, coverage=0.95):
+    """
+    Same as credible interval
+    https://stats.stackexchange.com/questions/240749/how-to-find-95-credible-interval
+
+    Parameters
+    ----------
+    posterior_density : xarray.DataArray, shape (n_time, n_position_bins) or
+        shape (n_time, n_x_bins, n_y_bins)
+    coverage : float, optional
+
+    Returns
+    -------
+    threshold : ndarray, shape (n_time,)
+
+    """
+    try:
+        posterior_density = posterior_density.stack(
+            z=["x_position", "y_position"]
+        ).values
+    except KeyError:
+        posterior_density = posterior_density.values
+    const = np.sum(posterior_density, axis=1, keepdims=True)
+    sorted_norm_posterior = np.sort(posterior_density, axis=1)[:, ::-1] / const
+    posterior_less_than_coverage = np.cumsum(
+        sorted_norm_posterior, axis=1) >= coverage
+    crit_ind = np.argmax(posterior_less_than_coverage, axis=1)
+    # Handle case when there are no points in the posterior less than coverage
+    crit_ind[posterior_less_than_coverage.sum(axis=1) == 0] = (
+        posterior_density.shape[1] - 1
+    )
+
+    n_time = posterior_density.shape[0]
+    threshold = sorted_norm_posterior[(
+        np.arange(n_time), crit_ind)] * const.squeeze()
+    return threshold
