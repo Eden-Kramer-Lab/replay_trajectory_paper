95116d2cb0d095c6bc414c0eb01f9731777afced
Eric Denovellis <edeno@bu.edu>
2021-03-16 20:12:19 -0700

    FIx missing argument

--- scripts/queue_cluster_jobs.py
+++ scripts/queue_cluster_jobs.py
@@ -25,6 +25,7 @@ def get_command_line_arguments():
     parser.add_argument('--threads_per_worker', type=int, default=16)
     parser.add_argument('--plot_ripple_figures', action='store_true')
     parser.add_argument('--exclude_interneuron_spikes', action='store_true')
+    parser.add_argument('--CA1', action='store_true')
     parser.add_argument('--overwrite', action='store_true')
 
     return parser.parse_args()
