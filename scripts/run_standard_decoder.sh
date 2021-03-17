7b4bbcbc08dc1b7831f6d26ccccaaa78a9d08f88
Eric Denovellis <edeno@bu.edu>
2021-03-17 12:06:10 -0700

    Add script for running standard decoder

---
+++ scripts/run_standard_decoder.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+python queue_cluster_jobs_standard_decoder.py
+
+python queue_cluster_jobs_standard_decoder.py --Animal 'remy' --Day 35 --Epoch 2
+
+python queue_cluster_jobs_standard_decoder.py --Animal 'remy' --Day 35 --Epoch 4
+
+python queue_cluster_jobs_standard_decoder.py --Animal 'remy' --Day 36 --Epoch 2
+
+python queue_cluster_jobs_standard_decoder.py --Animal 'remy' --Day 36 --Epoch 4
+
+python queue_cluster_jobs_standard_decoder.py --Animal 'remy' --Day 37 --Epoch 2
+
+python queue_cluster_jobs_standard_decoder.py --Animal 'remy' --Day 37 --Epoch 4
